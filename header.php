cab4a8545509262abe1259bdcb34b42ee33ccf3a
Montekarl <55535813+Montekarl@users.noreply.github.com>
2019-09-19 12:47:02 +0100

    Add files via upload
     PSR-2

---
+++ header.php
@@ -0,0 +1,14 @@
+<nav class="navbar navbar-inverse"> 
+    <div class="container-fluid" style="display: inline-block !important;">
+        <ul class="nav navbar-nav">     
+            <li >
+                <a href="svieslente.php">Švieslentė</a>
+            </li>
+            <li >
+                <a href="index.php">Naujas Klientas</a>
+            </li>
+            <li >
+                <a href="aptarnavimas.php">Specialisto Puslapis</a>
+            </li>
+    </div>
+</nav>
