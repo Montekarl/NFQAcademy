66fda54b05bdeffbfafd98f1a4a916e00e587be1
Montekarl <55535813+Montekarl@users.noreply.github.com>
2019-09-26 17:49:30 +0100

    Changed database configuration for remote database
    $host = "us-cdbr-iron-east-02.cleardb.net";
    $user = "b0d30ac5761d76";
    $password = "08df4f57";
    $datbase = "heroku_6b4d1af85eae168";
    $conn=mysqli_connect($host,$user,$password);
    mysqli_select_db($conn,$datbase);
    include "header.php";

--- dbconfig.php
+++ dbconfig.php
@@ -1,9 +1,9 @@
 <?php
 
-$host = "localhost";
-$user = "root";
-$password = "";
-$datbase = "nfq";
+$host = "us-cdbr-iron-east-02.cleardb.net";
+$user = "b0d30ac5761d76";
+$password = "08df4f57";
+$datbase = "heroku_6b4d1af85eae168";
 $conn=mysqli_connect($host,$user,$password);
 mysqli_select_db($conn,$datbase);
 include "header.php";
